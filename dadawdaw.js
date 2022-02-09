6fa8a018d39d7236676e9ffcd069c735c3c7f965
99063304 <70692200+99063304@users.noreply.github.com>
2022-02-09 09:10:27 +0100

    First Commit

---
+++ dadawdaw.js
@@ -0,0 +1,41 @@
+
+
+ num = num+ '';
+var digits = num.split('');
+
+ case = {120, 121, 122, 123, 124, 125};
+ var boring = [];
+
+
+input.length;
+3
+5 15
+120 125
+779 783
+
+
+function start(input) {
+  for (var i = array[0]; i < array[1]; i++) {
+
+  }
+}
+
+function countBorings(case) {
+  for (var i = 0; i < case.length; i++) {
+
+    num = case[i] + '';
+    var digits = num.split('');
+
+    for (var d = 0; d < digits.length; d++) {
+
+      if (Math.abs(digits[i] % 2) == 1) {
+        boring.push(digits[i]);
+      }else {
+        return false;
+      }
+
+
+   }
+  }
+  return true;
+}
---
+++ script.js
@@ -0,0 +1,22 @@
+var slaoverknop = document.getElementById('slaoverknop');
+slaoverknop.value ='slaoverknop';
+
+var eensknop = document.getElementById('eensknop');
+eensknop.value = 'eensknop';
+
+var oneensknop = document.getElementById('oneensknop');
+oneensknop.value = 'oneensknop';
+
+var geenvanbeideknop = document.getElementById('geenvanbeideknop');
+geenvanbeideknop.value = 'geenvanbeideknop';
+
+
+
+
+
+
+
+
+function functionName() {
+  
+}
