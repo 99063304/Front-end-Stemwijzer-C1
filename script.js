1fdb074f1ed498eec1b648c2217af6deb90ca9d2
99063304 <70692200+99063304@users.noreply.github.com>
2022-02-23 10:39:06 +0100

    Final Commit After - Feedback

--- script.js
+++ script.js
@@ -1,136 +1,4 @@
-var parties = [{
-		name: "PVV",
-		secular: true,
-		size: 20,
-		long: "Partij voor de Vrijheid"
-	},
-	{
-		name: "D66",
-		secular: true,
-		size: 19,
-		long: "Democratie 66"
-	},
-	{
-		name: "CU",
-		secular: false,
-		size: 6,
-		long: 'Christen Unie'
-	},
-	{
-		name: "SP",
-		secular: true,
-		size: 14,
-		long: "Socialistische Partij"
-	}
-];
 
-// Testen op correcte match, voer in:
-// PVV: contra, pro, contra, pro
-// SP: pro, contra, contra, contra
-// D66: pro, contra, pro, contra
-// CU: pro, pro, pro, contra
-
-var subjects = [{
-		"title": "Bindend referendum",
-		"statement": "Er moet een bindend referendum komen, waarmee burgers door het parlement aangenomen wetten kunnen tegenhouden.",
-		"parties": [{
-				"name": "PVV",
-				"position": "contra",
-				"opinion": "Geen toelichting gegeven"
-			},
-			{
-				"name": "SP",
-				"position": "pro",
-				"opinion": "Directe inspraak en zeggenschap van mensen maakt onze democratie sterker en de besluiten beter. In plaats van een raadgevend referendum willen we zo snel mogelijk een correctief referendum, zodat de bevolking ook tussen de verkiezingen door de gekozen vertegenwoordigers kan corrigeren, zowel bij lokale, nationale als Europese onderwerpen."
-			},
-			{
-				"name": "D66",
-				"position": "pro",
-				"opinion": "D66 wil de democratische controle moderniseren. Daarom wil D66 kiezers een noodrem geven met de mogelijkheid om via een correctief bindend referendum wetsvoorstellen tegen te houden, nadat het parlement deze heeft aangenomen. Dit correctief bindend referendum omvat geen internationale verdragen."
-			},
-			{
-				"name": "CU",
-				"position": "pro",
-				"opinion": "Wij willen experimenteren met nieuwe vormen van directe democratie. Wij werken daarom aan alternatieve modellen om burgers te betrekken en hechten vooral aan het uitwisselen van argumenten en samen zoeken naar oplossingen. Tot dan steunen wij het referendum om burgers meer invloed te geven."
-			}
-		]
-	},
-	{
-		"title": "Maatschappelijke dienstplicht",
-		"statement": "Er moet een maatschappelijke dienstplicht voor jongeren komen. Zij kunnen dan dienen in het leger, bij de politie of in de zorg.",
-		"parties": [{
-				"name": "PVV",
-				"position": "pro",
-				"opinion": "Geen toelichting gegeven"
-			},
-			{
-				"name": "SP",
-				"position": "contra",
-				"opinion": "De SP is voorstander van een maatschappelijke stage voor jongeren binnen het onderwijs om kennis te maken met de maatschappij, bijvoorbeeld in zorg of onderwijs. Een maatschappelijke dienstplicht voor langere tijd buiten het onderwijs gaat echter veel te ver, zal veel kosten en weinig opleveren. Belangrijke maatschappelijke taken moeten worden verricht door mensen die daar gemotiveerd en goed opgeleid voor zijn,"
-			},
-			{
-				"name": "D66",
-				"position": "contra",
-				"opinion": "Natuurlijk is het mooi als jongeren zich willen inzetten voor de maatschappij, maar dat is het juist als zij er zÃ©lf voor kiezen. Daarom wil D66 dit niet verplicht stellen. Scholen kunnen kiezen voor het aanbieden van een maatschappelijke stage, maar dat moet niet door de overheid worden opgelegd."
-			},
-			{
-				"name": "CU",
-				"position": "pro",
-				"opinion": "Mensen nemen op allerlei manieren deel aan de samenleving: door te werken, te leren, vrijwilligerswerk te doen, buren of familie te helpen. Een maatschappelijke dienstplicht doet geen recht aan de verschillende manieren waarop mensen participeren en beperkt de individuele vrijheid te veel."
-			}
-		]
-	},
-	{
-		"title": "Anoniem solliciteren",
-		"statement": "Om discriminatie op basis van de naam te voorkomen, moet anoniem solliciteren bij de overheid en bij openbare instellingen de regel worden.",
-		"parties": [{
-				"name": "PVV",
-				"position": "contra",
-				"opinion": "Geen toelichting gegeven"
-			},
-			{
-				"name": "SP",
-				"position": "contra",
-				"opinion": "Discriminatie  moet fel bestreden worden in alle delen van de maatschappij. Experimenten met anoniem solliciteren uitbreiden is daarom een goed idee. Dit algeheel doorvoeren gaat nu echter een stap te ver. Bovendien zit anoniem solliciteren ook een actief diversiteitsbeleid juist in de weg."
-			},
-			{
-				"name": "D66",
-				"position": "pro",
-				"opinion": "Nederlanders met een niet-westerse achtergrond zijn drie keer vaker werkloos dan Nederlanders met een westerse achtergrond. D66 wil dit verschil terugdringen. EÃ©n van de maatregelen die D66 wil nemen is het opzetten van experimenten met neutrale sollicitatie- en promotieprocedures."
-			},
-			{
-				"name": "CU",
-				"position": "pro",
-				"opinion": "Waar het gaat om vacatures van de overheid wordt anoniem solliciteren de norm. GroenLinks wil dat de overheid een goede afspiegeling vormt van de samenleving, zeker bij publieke topfuncties. "
-			}
-		]
-	},
-	{
-		"title": "Groepsbelediging",
-		"statement": "Belediging van groepen op grond van ras, godsdienst of geaardheid moet niet langer strafbaar zijn.",
-		"parties": [{
-				"name": "PVV",
-				"position": "pro",
-				"opinion": "Geen toelichting gegeven"
-			},
-			{
-				"name": "SP",
-				"position": "contra",
-				"opinion": "De strafbaarheid op belediging van groepen blijkt in de praktijk een nuttig middel om bijvoorbeeld antisemitisme of religieuze haatoproepen tegen homoseksuelen tegen te gaan."
-			},
-			{
-				"name": "D66",
-				"position": "contra",
-				"opinion": "D66 is voor vrije meningsuiting maar vindt dat het opzettelijk beledigen, discrimineren en haat zaaien tegen mensen vanwege het behoren tot een bepaalde bevolkingsgroep, niet past in onze samenleving."
-			},
-			{
-				"name": "CU",
-				"position": "contra",
-				"opinion": "Onze samenleving is de afgelopen vijftien jaar verruwd. Respectloos gedrag lijkt normaal te zijn geworden. GroenLinks stelt een harde grens: we pakken racisme en discriminatie keihard aan, zeker als het gekoppeld wordt aan geweld of het oproepen tot geweld, intimidatie en bedreigingen."
-			}
-		]
-	}
-];
 // variables worden aangemaakt
 var dividervalGp = 0;
 var dividervalSp = 0;
@@ -139,6 +7,7 @@ var eensCount = -2;
 var oneensCount = 0;
 var stellingCount = 0;
 var pastChoice = 0;
+var iCount = 0;
 var patroon = [];
 var newPartypointers = {};
 var partyPointers2 = {};
@@ -408,9 +277,12 @@ function gewichtVraag() {
 function Showresult() {
 	var overzicht = document.getElementById('overzicht');
   // Alle elementen worden verwijdert om ruimte te maken voor de resultaat
-	for (var v = 0; v <= overzicht.children.length + 15; v++) {
-		overzicht.removeChild(overzicht.lastChild);
-	}
+	// for (var v = 0; v <= overzicht.children.length + 15; v++) {
+	// 	overzicht.removeChild(overzicht.lastChild);
+	// }
+	while (overzicht.firstChild) {
+  overzicht.removeChild(overzicht.lastChild);
+}
   //// /// /
 
 	var element = document.createElement('h1');
@@ -423,13 +295,21 @@ function Showresult() {
 		if (new_array[i][1] != 0) {
 			var elemt = document.createElement('p');
 			if (new_array[i][0] != 'Niet Stemmers') {
-				elemt.innerHTML = i + 1 + '. ' + subjects[0]['parties'][i]['position'] + ',' + subjects[1]['parties'][i]['position'] + ',' + subjects[2]['parties'][i]['position'] + ',' + subjects[3]['parties'][i]['position'] + ': ' + new_array[i][0];
+				// var allText = i + 1 + '. ' + subjects[0]['parties'][i]['position'];
+				// for (var bb = 0; bb < subjects.length; bb++) {
+				// 	allText =  allText + ',' + subjects[bb]['parties'][i]['position'];
+				// }
+				// allText = allText+ ': ' + new_array[i][0];
+				elemt.innerHTML = i+iCount+' '+new_array[i][0];
 			} else {
-				elemt.innerHTML = i + 1 + '. ' + 'Geen stemmen: Niet Stemmers';
+				var iDoublecount = i+1
+				elemt.innerHTML = iDoublecount+ '. ' + 'Geen stemmen: Niet Stemmers';
+				iCount++;
 			}
 			overzicht.appendChild(elemt);
 		}
 	}
+
   // als de gebruiker geen stmmen heeft ingevuld dan krijgt die dit als resultaat
 	if (slaVraagOver == subjects.length) {
 		var slaVraagOverText = document.createElement('p');
